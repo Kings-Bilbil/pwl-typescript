b2c87942d689ec079d5a10f533bbc96fb9cab5c3
Kings-Bilbil <nabilnur556@gmail.com>
2026-02-25 04:46:36 +0000

    upd

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 function hello (nama: string, umur: number):string{
-    return `Selamat datang ${nama} di PWL umur andi ${umur}`;
+    return `Selamat datang ${nama} di PWL, Umur ${umur}`;
 }
-const mahasiswa="andi";
+const mahasiswa="Nabil";
 const umur: number = 19;
 console.log(hello(mahasiswa, umur));
