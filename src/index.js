76c3a1d1c81fcdc75febfbc0ce8e75ee021c9b9d
Kings-Bilbil <nabilnur556@gmail.com>
2026-02-25 04:42:09 +0000

    update

---
+++ src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello(nama, umur) {
+    return `Selamat datang ${nama} di PWL umur andi ${umur}`;
+}
+const mahasiswa = "andi";
+const umur = 19;
+console.log(hello(mahasiswa, umur));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function hello (nama: string, umur: number):string{
+    return `Selamat datang ${nama} di PWL umur andi ${umur}`;
+}
+const mahasiswa="andi";
+const umur: number = 19;
+console.log(hello(mahasiswa, umur));
